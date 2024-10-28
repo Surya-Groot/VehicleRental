896ee6214d1f5246c3947124cee3dcc7353e828a
git-groot <github437@gmail.com>
2024-10-28 10:27:48 +0530

    featur

--- Routes/vehicleRoute.js
+++ Routes/vehicleRoute.js
@@ -5,13 +5,13 @@ const comControle = require('../Controles/commenControle');
 const vechi = require('../Tables/vechicle')
 const feature = require('../Tables/features');
 
+route.post('/add', async (req, res) => { await comControle.createItem(req, res, vechi,'VHI') });
 
+route.get('/getsingle/:id', async (req, res) => { await comControle.getItemById(req, res, vechi) });
 
-route.get('/getsingle/:id', async (req, res) => {await comControle.getItemById(req, res, vechi) });
+route.put('/update/:id', async (req, res) => { await comControle.updateItem(req, res, vechi) });
 
-route.put('/update/:id', async (req, res) => {await comControle.updateItem(req, res, vechi) });
-
-route.get('/getAll', async (req, res) => {await comControle.getAllItems(req, res, vechi) });
+route.get('/getAll', async (req, res) => { await comControle.getAllItems(req, res, vechi) });
 
 route.post('/add/feature', async (req, res) => { await comControle.createItem(req, res, feature, 'FUT') });
 
