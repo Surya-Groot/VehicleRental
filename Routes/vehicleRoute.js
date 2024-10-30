90f9ee074bbd1e93db915e343114392faccb5790
git-groot <github437@gmail.com>
2024-10-30 13:03:31 +0530

    delete vehicle

--- Routes/vehicleRoute.js
+++ Routes/vehicleRoute.js
@@ -13,6 +13,8 @@ route.get('/getAll', async (req, res) => { await comControle.getAllItems(req, re
 
 route.put('/update/:id', async (req, res) => { await comControle.updateItem(req, res, vechi) });
 
+route.delete('/delete/:id',async(req,res)=>{await comControle.deleteItem(req,res,vechi)});
+
 route.post('/add/feature', async (req, res) => { await comControle.createItem(req, res, feature, 'FUT') });
 
 route.get('/getall/feature', async (req, res) => { await comControle.getAllItems(req, res, feature) });
