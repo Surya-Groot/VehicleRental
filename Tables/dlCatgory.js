84c32ee0290b4fc2a6a0b99309b826984a041ce4
git-groot <github437@gmail.com>
2024-11-09 12:32:26 +0530

    remove

--- Tables/dlCatgory.js
+++ Tables/dlCatgory.js
@@ -7,7 +7,7 @@ const dlCategory = new mongoose.Schema({
     uniqId: String,
     name: String,
     ABB:String,
-    status: { type: String, default: "'true" },
+    status: { type: String, default: "true" },
 })
 
 module.exports = mongoose.model('dlCategory', dlCategory);
